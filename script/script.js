928042b5fbbcbe7c3cac275def16132b6a173f36
Guillaume Perrin <gperrin01@gmail.com>
2015-05-12 16:57:31 +0100

    more JS done, time to build function winners

--- script/script.js
+++ script/script.js
@@ -1,42 +1,79 @@
 $(document).ready(function(){
-  setUpEventListeners();
-})
+setUpEventListeners();
+}) // END DOC READY
 
-// THE CODE 
+// THE STEPS 
 var battlefield = {
   'row1': [1,2,3],
   'row2': [4,5,6],
   'row3': [7,8,9] 
 }
-var player1; var player2;
-var avatar1 = getPlayer_1_Avatar();
+var usedFields = [];
+var player1 = 'Player1';
+// NEXT var player1 = prompt('What is your name?'); 
+var player2 = 'Player2';
+var avatar1;
+getPlayer_1_Avatar();
 // NEXT: click on picture you want as avatar
 var avatar2 = avatar1==='X' ? 'O' : 'X';
-// NEXT: choose pic for the computer
-
+// NEXT: choose pic for player2
+var whoseTurn = player1;
 // NEXT: randomize who starts: equal chances = pure random
-var starter = Math.floor(Math.random()*10) % 2 ) ===0 ? player1 : player2;
-alert('Let us play! \n'+ starter+' will start! Click to choose your first move!');
+// whoseTurn = Math.floor(Math.random()*10) % 2 ) ===0 ? player1 : player2;
+// alert('Let us play! \n'+ whoseTurn+' will start! Click to choose your first move!');
+
+/* Steps: 
+Player1 clicks and it updates the battlefield with his name on the corresponding element
+Switch turns - player2 clicks and it updates with his name - but only if the area hasnt been clicked on before
+*/
 
 
-getPlayer_1_Move();
 
-// var player2 = getPlayer_2_Avatar()
-// random while this is computer
+// END OF STEPS
 
-// END OF CODE
+// EVENT LISTERNERS
 
-// THE FUNCTIONS
-var setUpEventListeners = function() {
+var setUpEventListeners = function(){
+  // UPDATES ARRAY WHEN WE CLICK
   $('.battlefield li').on('click', function(){
     // get coord of where i clicked
     var x = getXAxis($(this));
     var y = getYAxis($(this));
-    battlefield[x][y];
-    console.log(battlefield[x][y]);
+    // debugger;
+    // only perform action if this area hasnt been clicked on before
+    if ( usedFields.indexOf($(this).attr('id')) === -1 ) {
+      if (whoseTurn === player1) {
+        console.log('battlefield[x][y] is ', battlefield[x][y]);
+        usedFields.push($(this).attr('id'));
+        battlefield[x][y] = player1;
+        $(this).append(avatar1);
+        // this is pushing the whole <li> down !!!
+        whoseTurn = player2;
+        console.log('player is ', player1);
+        console.log('usedFields are ', usedFields);
+      } else {
+        console.log('battlefield[x][y] is ', battlefield[x][y]);
+        usedFields.push($(this).attr('id'));
+        battlefield[x][y] = player2;
+        $(this).append(avatar2);
+        whoseTurn = player1;
+        console.log('player is ', player2);
+        console.log('usedFields are ', usedFields);        
+      }
+    } else {
+     alert('Click an available zone')
+   }     
   })
-}
 
+} // End Listerners
+
+// THE FUNCTIONS
+function getPlayer_1_Avatar() {
+  avatar1 = prompt('Hey '+player1+ ', are you the X or the O type?');
+}
+function getPlayer_2_Avatar() {
+  avatar2 = prompt('Hey '+player2+ ', are you the X or the O type?');
+}
 function getXAxis ($item) {
   // X axis in battlefield oject = id of the parent <ul>
   return $item.parent().attr('id');
@@ -45,11 +82,7 @@ function getYAxis ($item) {
   // Y axis is the Nth <li> of its siblings)
   return $item.index();
 }
-function getPlayer_1_Avatar() {
-  prompt('Are you the X or the O type?');
-  // Add random to see who starts
 
-}
-function getPlayer_1_Move() {
-  alert('CLick')
-}
+
+
+

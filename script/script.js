81043f0bbdedf699a23a1cc09633ddbaf7f0e4e1
Guillaume Perrin <gperrin01@gmail.com>
2015-05-12 14:19:55 +0100

    more JS, planning the steps for UI

--- script/script.js
+++ script/script.js
@@ -1,46 +1,55 @@
+$(document).ready(function(){
+  setUpEventListeners();
+})
+
+// THE CODE 
 var battlefield = {
   'row1': [1,2,3],
   'row2': [4,5,6],
   'row3': [7,8,9] 
 }
+var player1; var player2;
+var avatar1 = getPlayer_1_Avatar();
+// NEXT: click on picture you want as avatar
+var avatar2 = avatar1==='X' ? 'O' : 'X';
+// NEXT: choose pic for the computer
 
-$(document).ready(function(){
-  setUpEventListeners();
-})
+// NEXT: randomize who starts: equal chances = pure random
+var starter = Math.floor(Math.random()*10) % 2 ) ===0 ? player1 : player2;
+alert('Let us play! \n'+ starter+' will start! Click to choose your first move!');
 
+
+getPlayer_1_Move();
+
+// var player2 = getPlayer_2_Avatar()
+// random while this is computer
+
+// END OF CODE
+
+// THE FUNCTIONS
 var setUpEventListeners = function() {
   $('.battlefield li').on('click', function(){
     // get coord of where i clicked
     var x = getXAxis($(this));
     var y = getYAxis($(this));
     battlefield[x][y];
+    console.log(battlefield[x][y]);
   })
 }
 
-var getXAxis = function($item) {
+function getXAxis ($item) {
   // X axis in battlefield oject = id of the parent <ul>
   return $item.parent().attr('id');
 }
-var getYAxis = function($item) {
+function getYAxis ($item) {
   // Y axis is the Nth <li> of its siblings)
   return $item.index();
 }
+function getPlayer_1_Avatar() {
+  prompt('Are you the X or the O type?');
+  // Add random to see who starts
 
-
-
-// PAST WAY
-//   $('#1_1').on('click', function(){
-//     var x = getXAxis($(this).attr('id'));
-//     var y = getYAxis($(this).attr('id'));
-//     debugger;
-//     console.log(battlefield[x][y] );
-//   })
-// }
-
-// var getXAxis = function(string) {
-//   var coord = string.split('');
-//   // generic way of taking coords if double-digits, eg 12_14
-//   return coord.slice(0,coord.indexOf('_')).join('');
-// }
-// var getYAxis = function(string) {
-//   return string.split('').slice(string.indexOf('_')+1).join('');
+}
+function getPlayer_1_Move() {
+  alert('CLick')
+}

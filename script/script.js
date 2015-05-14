54381edc32358e5eb0834933ed911624c4a1cbe0
Guillaume Perrin <gperrin01@gmail.com>
2015-05-14 14:52:14 +0100

    local storage works

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 var allAvatars = {
       'pacman': {'img': 'img/avatars/pacman.png', 'sound': '#'},
-      'pacwoman': {'img': 'img/avatars/pacwoanman.png', 'sound': '#'},
+      'pacwoman': {'img': 'img/avatars/pacwoman.png', 'sound': '#'},
       'ghost_red': {'img': 'img/avatars/ghost_red.png', 'sound': '#'},
       'ghost_yellow': {'img': 'img/avatars/ghost_yellow.png', 'sound': '#'},
 }
@@ -19,14 +19,14 @@ var player2;
 var whoseTurn;
 // animation on Play button to make it stand out, disappears on click
 
-/******************************* end Steps
+/*end Steps
 
 THE EVENT LISTERNERS  ***********************/
 
 function eventListenersInitGame() {
   // Click play to start the game, only after that the other buttons can respond
   $('#ready').on('click', function() {
-    // prevent further clicks on ready
+    // remove animation on click and prevent re-clickng
     $('#ready').off('click');
     allPlayers = allPlayersInitialise() ;
     player1 = allPlayers.player1;
@@ -71,7 +71,7 @@ function allPlayersInitialise() {
   var secondP = getPlayerName('Second');
   var secondExtras = getPlayerExtras(secondP);
   return {
-    'player1': {'name': firstP, 'avatar': firstExtras[0], 'sound': firstExtras[2], 'winCount': 0},
+    'player1': {'name': firstP, 'avatar': firstExtras[0], 'sound': firstExtras[1], 'winCount': 0},
     'player2': {'name': secondP, 'avatar': secondExtras[0], 'sound': secondExtras[1], 'winCount': 0}, 
     'whoseTurn': ''
      };
@@ -95,11 +95,123 @@ function getPlayerName(player) {
       break;
     default:
       alert("Please enter a valid key. Let's try again");
-      getPlayerAvatar(player);
+      getPlayerExtras(player);
+  }
+}
+function updateAvatars(){
+  $('#p1 .icon_avatar').css("background-image", "url("+player1.avatar+")");
+  $('#p1 .wincount p:first-child').text(player1.name);
+  $('#p2 .icon_avatar').css("background-image", "url("+player2.avatar+")");
+  $('#p2 .wincount p:first-child').text(player2.name);
+}
+function updateScoreboard() {
+  $('#p1 .wincount p:last-child').text('Wins: '+player1.winCount);
+  $('#p2 .wincount p:last-child').text('Wins: '+player2.winCount);
+}
+
+/***** Functions to Compute Moves **********/
+
+function getXAxis ($item) {
+  // X axis in battlefield oject = id of the parent <ul>
+  return $item.parent().attr('id');
+}
+function getYAxis ($item) {
+  // Y axis is the Nth <li> of its siblings)
+  return $item.index();
+}
+function computeAndCheckWinner() {
+  //ensure zone won't respond any more
+  $(this).off('click', computeAndCheckWinner);
+  // get coordinates of where i clicked
+  var x = getXAxis($(this));
+  var y = getYAxis($(this));
+  // only perform action if this area hasn't been clicked on before
+  // Record(which player, which move, which area), then switch turn
+  // if ( usedFields.indexOf($(this).attr('id')) === -1 ) {
+    if (whoseTurn === player1.name) {
+      computeMove(player1, x, y, $(this) );
+      checkWinner(player1,x,y);
+      whoseTurn = player2.name;
+      } 
+    else {
+      computeMove(player2, x, y, $(this) );
+      checkWinner(player2,x,y);
+      whoseTurn = player1.name;       
+      }
+}
+  // else { 
+  //   // alert('Click on an available zone')
+  // }     
+
+function computeMove(player, x, y, $item) {
+  // update usedFields with my move, and battlefield array with player.name
+  usedFields.push($item.attr('id'));
+  battlefield[x][y] = player.name;
+  $item.css("background-image", "url("+player.avatar+")");
+ // NEXT PLAY SOUND
+}
+
+/***** Functions to Check Winner **********/
+
+function checkWinner(player, x, y) {
+  if (isWinning(player, x, y)  ) {
+    player.winCount++;
+    updateScoreboard();
+    updateLocalStorage(player);
+    $('.battlefield li').off('click', computeAndCheckWinner);
+    alert(player.name + ' wins!');
+    // animation on Clear button to make it stand out, disappears on click
+  } else if (usedFields.length === 9){
+    return alert('Tie game!');
+    $('.battlefield li').off('click', computeAndCheckWinner);
+    // animation on Clear button to make it stand out
+    }
+}
+function isWinning(player, x, y) {
+  return isWinningRow(player, x, y) || isWinningColumn(player, x, y) || isWinningDiagonal(player, x, y);
+}
+function isWinningRow(player, x, y) {
+  // is current row all player.name?
+  return JSON.stringify([player.name, player.name, player.name]) === JSON.stringify(battlefield[x]);
+}
+function isWinningColumn(player, x, y) {
+  // column is defined by y, pass the 3 other rows
+  return JSON.stringify(battlefield['row1'][y]) === JSON.stringify(battlefield['row2'][y]) &&  JSON.stringify(battlefield['row2'][y]) === JSON.stringify(battlefield['row3'][y]);
+}
+function isWinningDiagonal(player, x, y) {
+  // only two possible diagonal combo
+  return ( JSON.stringify(battlefield['row1'][0]) === JSON.stringify(battlefield['row2'][1]) &&  JSON.stringify(battlefield['row2'][1]) === JSON.stringify(battlefield['row3'][2]) )
+    || (  JSON.stringify(battlefield['row1'][2]) === JSON.stringify(battlefield['row2'][1]) &&  JSON.stringify(battlefield['row2'][1]) === JSON.stringify(battlefield['row3'][0]) );
+}
+
+function updateLocalStorage(player) {
+  // if player doesnt exist in Storage, create it. If already exists, grab his winCount from local storage and one to it 
+  if ( localStorage.getItem(player.name) === null) {
+    var stringPlayer = JSON.stringify(player);
+    localStorage.setItem(player.name, stringPlayer );
+  } else {
+    // de-stringify the object 'player.name' which is in storage; 
+    var stringPlayer = localStorage.getItem(player.name);
+    var parsePlayer = JSON.parse(stringPlayer);
+    // add 1 to the winCount and update local storage with new value
+    parsePlayer.winCount++;
+    stringPlayer = JSON.stringify(parsePlayer)
+    localStorage.setItem(player.name, stringPlayer);
   }
 }
 
+/************ Reset Functions  *******/
 
+function resetBattlefield() {;
+  battlefield = battlefieldInitialise();
+  usedFields = [];
+  whoseTurn = player1.name;
+  // or random, or whoever started
+  $('.battlefield li').css("background-image", 'none');
+  // re-enable the listeners on the field and play buton
+  // BUG BUG when playing mulitple times
+  eventListenersPlayGame();
+}
 
 
 
@@ -135,110 +247,3 @@ function getPlayerName(player) {
 //       getPlayerAvatar(player);
 //   }
 // }
-
-
-function getPlayerSound(player) {
-
-}
-function updateAvatars(){
-  $('#p1 .icon_avatar').css("background-image", "url("+player1.avatar+")");
-  $('#p1 .wincount p:first-child').text(player1.name);
-  $('#p2 .icon_avatar').css("background-image", "url("+player2.avatar+")");
-  $('#p2 .wincount p:first-child').text(player2.name);
-}
-function updateScoreboard() {
-  $('#p1 .wincount p:last-child').text('Wins: '+player1.winCount);
-  $('#p2 .wincount p:last-child').text('Wins: '+player2.winCount);
-}
-
-/***** Functions to Compute Moves **********/
-
-function getXAxis ($item) {
-  // X axis in battlefield oject = id of the parent <ul>
-  return $item.parent().attr('id');
-}
-function getYAxis ($item) {
-  // Y axis is the Nth <li> of its siblings)
-  return $item.index();
-}
-function computeAndCheckWinner() {
-  // get coord of where i clicked
-  var x = getXAxis($(this));
-  var y = getYAxis($(this));
-  // only perform action if this area hasn't been clicked on before
-  // Record(which player, which move, which area), then switch turn
-  console.log(usedFields.indexOf($(this).attr('id')) === -1)
-  if ( usedFields.indexOf($(this).attr('id')) === -1 ) {
-    if (whoseTurn === player1.name) {
-      computeMove(player1, x, y, $(this) );
-      checkWinner(player1,x,y);
-      whoseTurn = player2.name;
-      } 
-    else {
-      computeMove(player2, x, y, $(this) );
-      checkWinner(player2,x,y);
-      whoseTurn = player1.name;       
-      }
-  }
-  else { 
-    alert('Click on an available zone')
-  }     
-}
-function computeMove(player, x, y, $item) {
-  // update usedFields with my move, and battlefield array with player.name
-  usedFields.push($item.attr('id'));
-  battlefield[x][y] = player.name;
-  $item.css("background-image", "url("+player.avatar+")");
- // NEXT PLAY SOUND
-}
-
-/***** Functions to Check Winner **********/
-
-function checkWinner(player, x, y) {
-  if (isWinning(player, x, y)  ) {
-    player.winCount++;
-    updateScoreboard();
-    // prevent further clicks on the field
-    $('.battlefield li').off('click');
-    alert(player.name + ' wins!');
-    // animation on Clear button to make it stand out, disappears on click
-  } else if (usedFields.length === 9){
-    $('.battlefield li').off('click');
-    return alert('Tie game!');
-    // animation on Clear button to make it stand out
-    }
-}
-function isWinning(player, x, y) {
-  return isWinningRow(player, x, y) || isWinningColumn(player, x, y) || isWinningDiagonal(player, x, y);
-}
-function isWinningRow(player, x, y) {
-  // is current row all player.name?
-  return JSON.stringify([player.name, player.name, player.name]) === JSON.stringify(battlefield[x]);
-}
-function isWinningColumn(player, x, y) {
-  // column is defined by y, pass the 3 other rows
-  return JSON.stringify(battlefield['row1'][y]) === JSON.stringify(battlefield['row2'][y]) &&  JSON.stringify(battlefield['row2'][y]) === JSON.stringify(battlefield['row3'][y]);
-}
-function isWinningDiagonal(player, x, y) {
-  // only two possible diagonal combo
-  return ( JSON.stringify(battlefield['row1'][0]) === JSON.stringify(battlefield['row2'][1]) &&  JSON.stringify(battlefield['row2'][1]) === JSON.stringify(battlefield['row3'][2]) )
-    || (  JSON.stringify(battlefield['row1'][2]) === JSON.stringify(battlefield['row2'][1]) &&  JSON.stringify(battlefield['row2'][1]) === JSON.stringify(battlefield['row3'][0]) );
-}
-
-/************ Reset Functions  *******/
-
-function resetBattlefield() {;
-  battlefield = battlefieldInitialise();
-  usedFields = [];
-  whoseTurn = player1.name;
-  // or random, or whoever started
-  $('.battlefield li').css("background-image", 'none');
-  // re-enable the listeners on the field and play buton
-  eventListenersPlayGame();
-}
-
-// Reset players
-  // $('#p1 .icon_avatar, #p2 .icon_avatar').css("background-image", 'none');
-
-
-
